0f61ba0008a00606bda5bb90985a42d6a9d69552
AasprillaCucalon <achodeiyank@gmail.com>
2026-05-16 16:24:31 +0200

    Configurar Express para usar EJS

--- src/app.js
+++ src/app.js
@@ -12,6 +12,16 @@ import { getRssFeed } from "./controllers/rss.controller.js";
 
 const app = express();
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Configuración de EJS
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
